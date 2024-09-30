021d0d6090cc60f86b6f691e122cd46aadb0eded
arpita733 <arpita.cse2005@gmail.com>
2024-09-30 13:45:53 +0530

    New Project setup lab003#

--- src/Task001/Lab001.py
+++ src/Task001/Lab001.py
@@ -1,3 +1,1 @@
-print("Hello Pramod, Welcome to the Learning Python.")
-print(2+2)
-print(2-2)
+print("hello PYTHON")
---
+++ src/Task001/lab003#.py
@@ -0,0 +1,9 @@
+print("hello PYTHON")
+a = input("ENTER THE VALUE OF THE FIRST NUMBER\n")
+b = input("ENTER THE VALUE OF THE SECOND NUMBER\n")
+if a > b:
+    print("The first number is greater")
+elif a < b:
+    print("The second number is greater")
+else:
+    print("Two Numbers numbers are equal")
